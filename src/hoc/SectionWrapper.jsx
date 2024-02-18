73fffd2d61c1314de38704b2e816970f2dcc03ee
Alindra Putra <alindrapu@gmail.com>
2024-02-19 02:37:06 +0700

    [add] section wrapper index export and styling, [fix] error import hoc sectionwrapper

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -1,7 +1,21 @@
-import { motion } from "framer-motion";
+import { motion, stagger } from "framer-motion";
 
-const SectionWrapper = () => {
-  return <div>SectionWrapper</div>;
-};
+import { styles } from "../styles";
+import { staggerContainer } from "../utils/motion";
+
+const SectionWrapper = (Component, idName) =>
+  function HOC() {
+    return (
+      <motion.section
+        variants={staggerContainer}
+        initial="hidden"
+        whileInView={"show"}
+        viewport={{ once: true, amount: 0.25 }}
+        className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
+      >
+        <Component />
+      </motion.section>
+    );
+  };
 
 export default SectionWrapper;
